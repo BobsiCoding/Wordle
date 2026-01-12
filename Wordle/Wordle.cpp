b40b4e8f5f7689b159def2771b1bb2aab61e5341
BobsiCoding <nikolov.nik.boyan@gmail.com>
2026-01-13 00:52:57 +0200

    New functionality - player registry and bug fixes
    Развита е опция номер две от списъка в главното меню за регистрация на нов играч. Създадени са 6 нови функции за тази цел, позволяващи достъп до списъка с потребители. Също така е подобрена проверката на данните при избор на действие от главното меню

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -2,6 +2,31 @@
 #include <fstream>
 #include <cstdlib>
 
+void mainMenu();
+int mySizeOf(const char* str);
+int myChoice();
+bool blankSpacesCheck(char* str);
+bool stringsEqual(const char* a, const char* b);
+bool usernameExists(const char* str, const char* filepath);
+void rightUsersData(char* username, char* password);
+void registerUser(const char* filepath);
+
+const int DATA_SIZE = 16;
+
+int main()
+{
+    mainMenu();
+    int user_choice = myChoice();
+    char filepath[] = { "users.txt" };
+    if (user_choice == 1) {
+
+    }
+    else if (user_choice == 2) {
+        registerUser(filepath);
+    }
+
+}
+
 void mainMenu() {
     std::cout << "\t    Main menu "<<std::endl;
     std::cout << "Please choose one of the following options: " << std::endl;
@@ -11,12 +36,26 @@ void mainMenu() {
     std::cout << "Your choice: " << std::endl;
 }
 
+int mySizeOf(const char* str) {
+    if (str == nullptr) {
+        std::cout << "Nullpointer error.";
+        return 0;
+    }
+    int cnt = 0;
+    while (*str != '\0') {
+        cnt++;
+        str++;
+    }
+    return cnt;
+}
+
 int myChoice() {
-    int choice = 0;
+    const int INPUTSIZE = 16;
+    char choice[INPUTSIZE];
     std::cin >> choice;
     bool validChoice = false;
     while (!validChoice) {
-        if (choice < 1 || choice > 3) {
+        if (*choice < '1' || *choice > '3' || mySizeOf(choice) > 1) {
             std::cout << "Not a valid input. Try aggain: "<<std::endl;
             std::cout << "Please enter your choice: ";
             std::cin >> choice;
@@ -25,11 +64,152 @@ int myChoice() {
             validChoice = true;
         }
     }
-    return choice;
+    return *choice - '0';
 }
 
-int main()
-{
-    mainMenu();
-    int choice = myChoice();
+bool blankSpacesCheck(char* str) {
+    if (str == nullptr) {
+        std::cout << "Nullpointer error.";
+        return 0;
+    }
+    while (*str != '\0') {
+        if (*str == ' ' || *str == '\t')
+            return 0;
+        str++;
+    }
+    return 1;
+
 }
+
+bool stringsEqual(const char* str1, const char* str2) {
+    while (*str1 != '\0' && *str2 != '\0') {
+        if (*str1 != *str2)
+            return false;
+        str1++;
+        str2++;
+    }
+    if (mySizeOf(str1) != mySizeOf(str2))
+        return false;
+    return true;
+}
+    
+bool usernameExists(const char* username, const char* filepath) {
+    if (username == nullptr || filepath == nullptr) {
+        std::cout << "Nullpointer error.";
+        return 0;
+    }
+    std::ifstream ifs(filepath);
+    if (!ifs.is_open()) {
+        std::cout << "A connection with the database could not be established." << std::endl;
+        std::cout << "Please close the program and try again.";
+        return 0;
+    }
+
+    char u[DATA_SIZE];
+    char p[DATA_SIZE];
+
+    while (ifs >> u >> p) {
+        if (stringsEqual(u, username)) {
+
+            ifs.close();
+            return true;
+        }
+    }
+    ifs.close();
+    return false;
+}
+
+void rightUsersData(char* username, char* password) {
+    if (username == nullptr || password == nullptr) {
+        std::cout << "Nullpointer error.";
+        return;
+    }
+    
+    std::cout << "Username: ";
+    std::cin >> std::ws;
+    std::cin.getline(username, DATA_SIZE);
+    std::cout << std::endl;
+    bool rightUsername = false;
+
+    while (!rightUsername) {
+        if (mySizeOf(username) > 15 || mySizeOf(username) == 0 ) {
+            std::cout << "The username must be under 16 characters." << std::endl;
+            std::cout << "Username:";
+            std::cin.getline(username, DATA_SIZE);
+            std::cout << std::endl;
+            continue;
+        }
+
+        if (!(blankSpacesCheck(username))) {
+            std::cout << "The username must not contain blank spaces." << std::endl;
+            std::cout << "Username:";
+            std::cin.getline(username, DATA_SIZE);
+            std::cout << std::endl;
+            continue;
+        }
+        rightUsername = true;
+    }
+
+    std::cout << "Password: ";
+    std::cin.getline(password, DATA_SIZE);
+    std::cout << std::endl;
+
+    bool rightPassword = false;
+
+    while (!rightPassword) {
+        if (mySizeOf(password) > 15 || mySizeOf(password) == 0) {
+            std::cout << "The password must be under 16 characters." << std::endl;
+            std::cout << "Password: ";
+            std::cin.getline(password, DATA_SIZE);
+            std::cout << std::endl;
+            continue;
+        }
+
+        if (!(blankSpacesCheck(password))) {
+            std::cout << "The password must not contain blank spaces." << std::endl;
+            std::cout << "Password: ";
+            std::cin.getline(password, DATA_SIZE);
+            std::cout << std::endl;
+            continue;
+        }
+        rightPassword = true;
+    }
+    std::cout << "Your username: " << username << " and password: " << password << std::endl;
+}
+
+void registerUser(const char* filepath) {
+    if (filepath == nullptr) {
+        std::cout << "Nullpointer error.";
+        return;
+    }
+
+    char username[DATA_SIZE];
+    char password[DATA_SIZE];
+
+    std::cout << "Please enter your username and password." << std::endl;
+    std::cout << "They must be under under 16 characters long. ";
+    std::cout << "They must not contain blank spaces. " << std::endl;
+
+    rightUsersData(username, password);
+
+
+    while (usernameExists(username, filepath)) {
+        std::cout << "This username is taken. Please choose a different one." << std::endl;
+        rightUsersData(username, password);
+    }
+
+    std::ofstream ofs(filepath, std::ios::app);
+
+    if (!ofs.is_open()) {
+        std::cout << "A connection with the database could not be established." << std::endl;
+        std::cout << "Please close the program and try again.";
+        return;
+    }
+
+    ofs << username << " " << password << std::endl;
+    ofs.close();
+
+    std::cout << "Registration successful!" << std :: endl;
+
+}
+
