bfd33b7d8c2f3f4c18b2ef2596afa5dc6018ae4c
BobsiCoding <nikolov.nik.boyan@gmail.com>
2026-01-12 20:21:16 +0200

    Added main menu
    Създадени за две нови функции, едната показваща основното меню при стартиране, а другата - въвеждане на избора.

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -1,7 +1,35 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+void mainMenu() {
+    std::cout << "\t    Main menu "<<std::endl;
+    std::cout << "Please choose one of the following options: " << std::endl;
+    std::cout << "1. Sign in as an existing user" << std::endl;
+    std::cout << "2. Register as a new user " << std::endl;
+    std::cout << "3. Exit " << std::endl;
+    std::cout << "Your choice: " << std::endl;
+}
+
+int myChoice() {
+    int choice = 0;
+    std::cin >> choice;
+    bool validChoice = false;
+    while (!validChoice) {
+        if (choice < 1 || choice > 3) {
+            std::cout << "Not a valid input. Try aggain: "<<std::endl;
+            std::cout << "Please enter your choice: ";
+            std::cin >> choice;
+        }
+        else {
+            validChoice = true;
+        }
+    }
+    return choice;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    mainMenu();
+    int choice = myChoice();
 }
